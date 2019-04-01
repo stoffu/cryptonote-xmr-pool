493a3635b07d7c37c52a75a7258150fe87c50700
stoffu <stoffu@protonmail.ch>
2019-04-01 15:29:37 +0900

    Aeon testnet pool hosted at 162.210.173.150

--- website-example/config.js
+++ website-example/config.js
@@ -1,17 +1,17 @@
-var api = "http://poolhost.com:1117";
+var api = "http://162.210.173.150:8117";
 
-var poolHost = "poolhost.com";
+var poolHost = "162.210.173.150";
 
 var irc = "irc.freenode.net/#aeon-pools";
 
-var email = "support@poolhost.com";
+var email = "stoffu@protonmail.ch";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorer = "http://162.210.173.151/block/{id}";
 
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorer = "http://162.210.173.151/tx/{id}";
 
 var themeCss = "themes/default-theme.css";
